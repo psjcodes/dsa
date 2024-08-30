d3fe50460f350f58ca772af87367da871286fc22
Pranay Jha <pranay.jha@gmail.com>
2024-08-30 11:03:46 -0700

    some knn fix

--- math/knn.py
+++ math/knn.py
@@ -4,6 +4,7 @@ from typing import List, Tuple, Any
 import numpy as np
 from numpy.typing import NDArray
 
+
 class MyKMinHeap:
 
     def __init__(self, k: int) -> None:
@@ -20,7 +21,7 @@ class MyKMinHeap:
     def pop(self) -> Any:
         _, item = heapq.heappop(self._heap)
         return item
-    
+
     def get_items(self) -> List[Any]:
         return [item for _, item in self._heap]
 
@@ -39,7 +40,9 @@ class MyKNN:
         self._k = k
         self._method = method
 
-    def _euclidean_distance(self, x: NDArray[np.float64], y: NDArray[np.float64]) -> np.float64:
+    def _euclidean_distance(
+        self, x: NDArray[np.float64], y: NDArray[np.float64]
+    ) -> np.float64:
         return np.dot(x - y, x - y)
 
     def _linear_search(self, input: NDArray[np.float64], top_k: MyKMinHeap) -> None:
@@ -47,20 +50,16 @@ class MyKNN:
             dist = self._euclidean_distance(x, input)
             top_k.push(item=i, priority=dist)
 
-    def knn_search(self, input: NDArray[np.float64]) -> dict[Any, int]:
+    def search(self, input: NDArray[np.float64]) -> dict[Any, int]:
         top_k = MyKMinHeap(k=self._k)
+
         if self._method == "linear":
             self._linear_search(input=input, top_k=top_k)
 
         knn_labels = [self._labels[i] for i in top_k.get_items()]
         count_labels = Counter(knn_labels)
-        return {label: count for label, count in count_labels.items()}
-    
+        return dict(count_labels.items())
+
     def classify(self, input: NDArray[np.float64]) -> Any:
-        knn = self.knn_search(input=input)
+        knn = self.search(input=input)
         return max(knn, key=knn.get)
-
-
-
-
-            
